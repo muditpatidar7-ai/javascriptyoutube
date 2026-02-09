16db1faea0cb5478727c6b07fed2502f0be928db
mudit patidar <muditpatidar7@gmail.com>
2026-02-09 16:41:25 +0000

    learned data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all js code as new version 
+
+// alert (3+3) // we are using nodejs, not browser this will show error 
+console.log(33
+    +
+    33) // this will work fine but code readability is very bad
+
+//data types in js
+let name = "mudit" // string
+let age = 22 // number
+let isloggedin= true // boolean
+let firstName = undefined // undefined
+let selectedColor = null // null    
+//number => 2 to the power of 53
+//bigint (it has big number) => 2 to the power of 53
+//string => ""(sequence of characters)
+//boolean => true or false
+//undefined => variable is declared but value is not assigned 
+//null => standalone value (empty value) => we can assign null to variable but we cannot assign undefined to variable
+//symbol => unique identifier (used in react)
+
+
+//objects 
+console.log(typeof "mudit");
+console.log(typeof 22);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof true);
+console.log(typeof firstName);
+console.log(typeof selectedColor);
+
