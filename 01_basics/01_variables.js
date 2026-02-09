4b49c83765c707e7a81bec1fd5afa2fb0b8581e8
mudit patidar <muditpatidar7@gmail.com>
2026-02-09 16:19:06 +0000

    learned variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountemail = "muditpatidar7@gmail.com"// variable
+var accountpassword = "22223" // variable
+let accountstate 
+accountcity = "jaipur"// ese bhi variable declare kr sakte h pr nhi krna chaiye 
+
+//accountId = 2 // mot allowed 
+accountemail = "karachiii.com"
+accountpassword = "222"
+accountcity = "pakistan"
+
+console.log(accountId);
+console.table([accountId,accountemail,accountpassword,accountcity,accountstate])
+/*
+prefer not to use var 
+because of issue in block scope and functional scope 
+*/
+
